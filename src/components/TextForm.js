a102c406f624be68b259fde48cca23775f9b86be
KVS SIVA KUMAR <sivakumar.k@comakit.com>
2023-01-08 10:32:29 +0530

    Completed to uppercase using state concept in react

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function (props) {
+  const handleUpClick = () => {
+    console.log("Uppercase");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    console.log("On Change");
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter Text Here");
+
   return (
     <>
       <div className="mb-3">
         <h1>{props.heading}</h1>
-        <textarea className="form-control" id="myBox" rows="5" />
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="5"
+        />
       </div>
-      <button className="btn btn-primary">Conver to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>
+        Conver to Uppercase
+      </button>
     </>
   );
 }
